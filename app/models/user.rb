8ba5b973d1cc07c1139f90ea19cf49a1d321934c
A　OKIYAMA <74709270+gurabushu@users.noreply.github.com>
2025-08-01 15:46:22 +0900

    devise,model,controller実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :img_url, presence: true, format: { with: /\Ahttps?:\/\/.*\.(?:png|jpg|jpeg|gif)\z/i }
+  validates :introduction, length: { maximum: 500 }
+  validates :skills, presence: true
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 120 }, allow_nil: true
 end
